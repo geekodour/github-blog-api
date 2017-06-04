3b94755f282ddbe82536feaa750d981a30a50d59
Rishi Giri <rushforlinux@gmail.com>
2017-06-04 19:57:10 +0530

    minor changes

--- examples/src/main.js
+++ examples/src/main.js
@@ -1,5 +1,14 @@
 var gitblog = require('../../lib/index.js');
-var blog = gitblog({username:'lukego',repo:'blog',author:'lukego'});
-blog.setPost({per_page:3});
-blog.fetchBlogPosts().then(e=>console.log(e));
-blog.fetchBlogPostComments(2).then(e=>console.log(e));
+
+var blog = gitblog({
+	username: 'lukego',
+	repo: 'blog',
+	author: 'lukego'
+});
+
+blog.setPost({
+	per_page: 3
+});
+
+blog.fetchBlogPosts().then(e => console.log(e));
+blog.fetchBlogPostComments(2).then(e => console.log(e));
--- test.js
+++ test.js
@@ -1,11 +1,19 @@
 import test from 'ava';
 import blog from './lib/index';
 
-let testBlog = blog({username:'geekodour',repo:'gitpushblog'});
+let testBlog = blog({username: 'lukego', author: 'lukego', repo: 'blog'});
 
+test('fetchBlog', async t => {
+	const blog = testBlog.fetchBlogPosts().then(r => r);
+	t.is(await (blog), await (blog));
+});
 
-test('bar', async t => {
-    const bar = testBlog.fetchBlogPosts().then(r=>r)
-    console.log(await bar);
-        //t.is(await bar, 'bar');
+test('setPost', async t => {
+	const post = testBlog.setPost({per_page: 3});
+	t.is(await (post), await (post));
+});
+
+test('blogComments', async t => {
+	const comments = testBlog.fetchBlogPostComments(2).then(r => r);
+	t.is(await (comments), await (comments));
 });
